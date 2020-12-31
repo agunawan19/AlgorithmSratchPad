747f80a4ae25e749ad560a660d56c54a858a558a
Andri Gunawan <agunawan@programmer.net>
2020-12-31 00:00:16 -0800

    Add empty class

---
+++ AlgorithmLibrary/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmLibrary
+{
+    public class MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            return double.MinValue;
+        }
+    }
+}
